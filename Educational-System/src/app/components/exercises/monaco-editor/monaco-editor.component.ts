62f1168b47722ede7e5ead7bb68679d4889f27ac
Nagy Ádám Richárd <nagyadam9415@gmail.com>
2019-03-12 23:12:25 +0100

    +nyelvválasztás + css

--- Educational-System/src/app/components/exercises/monaco-editor/monaco-editor.component.ts
+++ Educational-System/src/app/components/exercises/monaco-editor/monaco-editor.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-monaco-editor',
@@ -6,12 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./monaco-editor.component.css']
 })
 export class MonacoEditorComponent implements OnInit {
+
   editorOptions = {theme: 'vs-dark', language: 'javascript'};
   code = 'function x() {\nconsole.log("Hello world!");\n}';
 
-  constructor() { }
+  editorOption$ = new BehaviorSubject(this.editorOptions);
+
+  constructor() {
+    this.editorOption$.subscribe( options => {
+      this.editorOptions = null;
+      this.editorOptions = options;
+
+      console.log(this.editorOptions.language);
+    });
+   }
 
   ngOnInit() {
+  }
+
+  settingsChanged(event) {
+    this.editorOptions.language = document.getElementsByTagName('select')[0].value;
+    this.editorOption$.next(this.editorOptions);
+
+  }
+
+  editorChanged(event) {
+    //
   }
 
 }
