6563b1d8d6c680baf0ee385cb9b62e9870b97762
Nagy Ádám Richárd <nagyadam9415@gmail.com>
2019-05-13 16:37:17 +0200

    final resolving conflict

--- Educational-System/src/app/components/exercises/monaco-editor/monaco-editor.component.ts
+++ Educational-System/src/app/components/exercises/monaco-editor/monaco-editor.component.ts
@@ -44,9 +44,6 @@ export class MonacoEditorComponent implements OnInit {
 =======
     this.compileService.compile(this.code, this.selectedLanguage).subscribe( (response: any) => {
 >>>>>>> 29ae5f75feed64251b31af1cdeb07c5945eefa3d
-      console.log(response);
-      this.compilerResponse.nativeElement.value = response.result;
-    }, error => {
       console.log(error);
     });
     this.compiled = true;
