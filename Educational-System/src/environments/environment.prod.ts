501b68f55d910f5808e99c2ca97d05d50e6fcb55
Nagy Ádám Richárd <nagyadam9415@gmail.com>
2019-10-03 21:56:33 +0200

    new build

--- Educational-System/src/environments/environment.prod.ts
+++ Educational-System/src/environments/environment.prod.ts
@@ -1,5 +1,9 @@
 export const environment = {
   production: true,
   BaseUrl: "http://167.86.98.54:4001",
+  UploadImageUrl: "http://167.86.98.54:4002/uploadImage",
+  BaseImageUrl: "http://167.86.98.54:4002/images/",
+  UploadPdfUrl: "http://167.86.98.54:4003/uploadPdf",
+  BasePdfUrl: "http://167.86.98.54:4003/pdf/",
   CompilerUrl: "http://167.86.98.54:4000/compile"
 };
